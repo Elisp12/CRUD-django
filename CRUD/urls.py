da881917a5f7ff63b718555e10400eb70777759f
Elisp12 <eliseuspdam.1999@gmail.com>
2024-07-10 20:26:52 -0300

    deletar item

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views.lista_produto import lista_produto
 from .views.criar_produto import criar_produto
 from .views.atualizar_produto  import atualizar_produto
+from .views.deletar_produto import deletar_produto
 
 urlpatterns = [
     path('', lista_produto, name='lista_produto'),
     path('criar/produto/', criar_produto, name='criar_produto'),
-    path('atualizar/produto/<int:index>/', atualizar_produto, name='atualizar_produto')
+    path('atualizar/produto/<int:index>/', atualizar_produto, name='atualizar_produto'),
+    path('deletar/produto/<int:index>/', deletar_produto, name='deletar_produto')
 
 ]
--- CRUD/views/__init__.py
+++ CRUD/views/__init__.py
@@ -1,3 +1,4 @@
 from .lista_produto import *
 from .criar_produto import *
 from .atualizar_produto import *
+from .deletar_produto import *
